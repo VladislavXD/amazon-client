8af47568b74075cf77e39ed1319f930302257f8f
Vladislav <neivo@vladislav.dev>
2025-07-13 18:11:04 +0500

    update domain

--- src/app/constants/seo.constants.ts
+++ src/app/constants/seo.constants.ts
@@ -2,6 +2,6 @@ export const SITE_NAME = 'Azura by VladislavDev | Next.js 14 | TypeScript 5.8.3 
 export const SITE_DESCRIPTION = 'Azura is a modern e-commerce platform built with Next.js 14, TypeScript 5.8.3, and Tailwind CSS 3.4.2. Explore a wide range of products, enjoy a seamless shopping experience, and stay updated with the latest trends.';
 export const SITE_KEYWORDS = 'Azura, e-commerce, Next.js, TypeScript, Tailwind CSS, online shopping, modern web development, VladislavDev';
 export const SITE_AUTHOR = 'VladislavDev';
-export const SITE_URL = 'https://the-amazon.vercel.app';
+export const SITE_URL = 'https://the-azura.vercel.app';
 
 export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
